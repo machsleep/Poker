f094ad22ff26c6f359e41754114475b02e334e0e
machsleep <machlseep@gmail.com>
2013-11-17 12:50:28 -0800

    Added some comments in Player.hpp

--- src/Player/Player.hpp
+++ src/Player/Player.hpp
@@ -15,12 +15,55 @@
 
 class Player {
 	public:
+		/**
+		 * Constructor for class.
+		 * Clients can specify as many arguments as needed. The default values for a constructor are:
+		 * name = "", chairIndex = 0, money = 0, folded, active, mucked = false.
+		 * It's important to initialize a Player's state correctly for appropriate handling from other modules (e.g., Rules).
+		 *
+		 * <p>
+		 * @param std::string name, the name of the player
+		 * @param bool chair, the chair index the player is sitting at 0-9 for most games.
+		 * @param bool folded, true if the player has folded, and false otherwise.
+		 * @param bool active, true if the player is active at a table (not sitting out)
+		 * @param bool mucked, true if a player mucks his/her cards.
+		 */
 		Player(std::string name="", unsigned int chair=0, double money=0, bool folded=false, bool active=false, bool mucked=false);
+
 		virtual ~Player();
+
+		/**
+		 * Returns the name of the poker player.
+		 * <p>
+		 * @return const std::string&, name
+		 */
 		virtual const std::string& getName() const;
+
+		/**
+		 * Returns the money available to the poker player.
+		 * <p>
+		 * @return const double money, money in DOLLARS.CENTS (e.g., $14.23)
+		 */
 		virtual const double getMoney() const;
+
+		/**
+		 * Returns true if a player is playing in a game. A player is playing in a game
+		 * if they have not folded and are active. In other words, the player can place a bet.
+		 * <p>
+		 * @return const bool
+		 */
 		virtual const bool isPlaying() const;
+
+		/**
+		 * Returns true if a player is sitting out, false otherwise. (e.g., not active)
+		 * <p>
+		 * @return const bool
+		 */
 		virtual const bool isSittingOut() const;
+
+		/**
+		 * Returns true if a player mucked his cards
+		 */
 		virtual const bool doesMuck() const;
 		virtual const bool isFolding() const;
 		virtual void addCardToHand(Card card);
@@ -30,7 +73,6 @@ class Player {
 		std::string name;
 		std::vector<Card> hand;
 		unsigned int chairIndex;
-		// US dollar.cents
 		double money;
 		bool folded;
 		bool active;
